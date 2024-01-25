ec2b6061342ebed2d3acfe60e44d90bb7df5d8c3
Ville <ville.reima@tietoa.fi>
2024-01-25 11:01:40 +0200

    Deploy settings

--- back/back/main.py
+++ back/back/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from loguru import logger
 
 app = FastAPI()
+
+logger.debug("> Aloitus.")
 
 
 @app.get("/")
 def root():
+    logger.debug("> root().")
     return {"message": "Hello!"}
